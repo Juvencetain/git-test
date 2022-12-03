d8893eca80e9e633c2b7b0f7895476221c194861
luonan <424261131@qq.com>
2022-12-04 02:07:24 +0800

    push test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -10,5 +10,6 @@ public class test {
         System.out.println("test4");
         System.out.println("master-fix test5");
         System.out.println("hot-fix test5");
+        System.out.println("push test6");
     }
 }
